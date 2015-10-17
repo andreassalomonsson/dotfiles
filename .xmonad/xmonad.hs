76ba6438144f25fb3357cd3c5f7adbb43f3ede77
Andreas Salomonsson <andreasalomonsson@gmail.com>
2015-10-17 22:22:20 +0200

    Adds XMonad config

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,23 @@
+import XMonad
+import XMonad.Actions.CycleWS
+import XMonad.Hooks.SetWMName
+import XMonad.Layout.ShowWName
+import qualified Data.Map as M
+
+main = xmonad $ defaultConfig
+    { startupHook = setWMName "LG3D"
+    , layoutHook = showWName $ layoutHook defaultConfig
+    , keys = myKeys <+> keys defaultConfig
+    }
+
+myKeys conf@(XConfig { XMonad.modMask = modmask }) = M.fromList
+    [ ((modmask,               xK_l), nextWS)
+    , ((modmask,               xK_h), prevWS)
+    , ((modmask .|. shiftMask, xK_l), shiftToNext)
+    , ((modmask .|. shiftMask, xK_h), shiftToPrev)
+    , ((modmask,               xK_k), nextScreen)
+    , ((modmask,               xK_j), prevScreen)
+    , ((modmask .|. shiftMask, xK_k), shiftNextScreen)
+    , ((modmask .|. shiftMask, xK_j), shiftPrevScreen)
+    , ((modmask,               xK_z), toggleWS)
+    ]
