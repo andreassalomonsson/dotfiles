d47c8d037f4c2e3e1adc3ae9063fdcafcf5c3772
Andreas Salomonsson <andreas.k.m.salomonsson@gmail.com>
2018-10-09 17:52:09 +0200

    Updates xmonad config with Programmer Dvorak key bindings and sets mod key to Windows key

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,24 +2,20 @@ import XMonad
 import XMonad.Actions.CycleWS
 import XMonad.Hooks.SetWMName
 import XMonad.Layout.ShowWName
+import XMonad.StackSet
 import qualified Data.Map as M
 
 main = xmonad $ def
     { startupHook = setWMName "LG3D"
     , layoutHook = showWName $ layoutHook def
+    , modMask = mod4Mask
     , keys = myKeys <+> keys def
     , normalBorderColor = "#073642"
     , focusedBorderColor = "#839496"
     }
 
 myKeys conf@(XConfig { XMonad.modMask = modmask }) = M.fromList
-    [ ((modmask,               xK_l), nextWS)
-    , ((modmask,               xK_h), prevWS)
-    , ((modmask .|. shiftMask, xK_l), shiftToNext)
-    , ((modmask .|. shiftMask, xK_h), shiftToPrev)
-    , ((modmask,               xK_k), nextScreen)
-    , ((modmask,               xK_j), prevScreen)
-    , ((modmask .|. shiftMask, xK_k), shiftNextScreen)
-    , ((modmask .|. shiftMask, xK_j), shiftPrevScreen)
-    , ((modmask,               xK_z), toggleWS)
-    ]
+    [((m .|. modmask, k), windows $ f i)
+        | (i, k) <- zip (XMonad.workspaces conf) [xK_ampersand, xK_bracketleft, xK_braceleft, xK_braceright, xK_parenleft
+                                                 ,xK_equal, xK_asterisk, xK_parenright, xK_plus, xK_bracketright, xK_exclam]
+        , (f, m) <- [(view, 0), (shift, shiftMask)]]
