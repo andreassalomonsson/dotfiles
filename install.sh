37127bf0b25c570c59befbb4f92fdddbcc00d6e1
Andreas Salomonsson <andreas.k.m.salomonsson@gmail.com>
2019-01-02 00:46:02 +0100

    Adds configuration for i3wm

--- install.sh
+++ install.sh
@@ -2,17 +2,24 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-ln -s ${DIR}/.inputrc ~/.inputrc && \
-
-ln -s ${DIR}/.zshrc ~/.zshrc && \
-ln -s ${DIR}/.zsh ~/.zsh && \
-
-ln -s ${DIR}/.tmux.conf ~/.tmux.conf && \
-
-ln -s ${DIR}/.vimrc ~/.vimrc && \
-
-ln -s ${DIR}/.Xresources ~/.Xresources && \
-
-rm -rf ~/.xmonad; ln -s ${DIR}/.xmonad ~/.xmonad && \
-
+ln -s ${DIR}/.inputrc ~/.inputrc
+ln -s ${DIR}/.vimrc ~/.vimrc
 ln -s ${DIR}/.gitconfig ~/.gitconfig
+
+command -v zsh > /dev/null 2>&1 && {
+    ln -s ${DIR}/.zshrc ~/.zshrc && \
+    ln -s ${DIR}/.zsh ~/.zsh
+}
+
+command -v tmux > /dev/null 2>&1 && {
+    ln -s ${DIR}/.tmux.conf ~/.tmux.conf
+}
+
+command -v xmonad > /dev/null 2>&1 && {
+    ln -s ${DIR}/.Xresources ~/.Xresources && \
+    rm -rf ~/.xmonad; ln -s ${DIR}/.xmonad ~/.xmonad
+}
+
+command -v i3 > /dev/null 2>&1 && {
+    rm -rf ~/.config/i3; ln -s ${DIR}/.config/i3 ~/.config/i3
+}
