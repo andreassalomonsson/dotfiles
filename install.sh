0a44f01578993797dd9ca9d109b444e080f1d5c5
Andreas Salomonsson <andreas.k.m.salomonsson@gmail.com>
2019-12-21 17:25:50 +0100

    Fixes broken path in install script

--- install.sh
+++ install.sh
@@ -2,28 +2,28 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-ln -s ${DIR}/.inputrc ~/.inputrc
-ln -s ${DIR}/.vimrc ~/.vimrc
-ln -s ${DIR}/.gitconfig ~/.gitconfig
+ln -s "$DIR/.inputrc" ~/.inputrc
+ln -s "$DIR/.vimrc" ~/.vimrc
+ln -s "$DIR/.gitconfig" ~/.gitconfig
 
 command -v zsh > /dev/null 2>&1 && {
-    ln -s ${DIR}/.zshrc ~/.zshrc && \
-    ln -s ${DIR}/.zsh ~/.zsh
+    ln -s "$DIR/.zshrc" ~/.zshrc && \
+    ln -s "$DIR/.zsh" ~/.zsh
 }
 
 command -v tmux > /dev/null 2>&1 && {
-    ln -s ${DIR}/.tmux.conf ~/.tmux.conf
+    ln -s "$DIR/.tmux.conf" ~/.tmux.conf
 }
 
 command -v xmonad > /dev/null 2>&1 && {
-    ln -s ${DIR}/.Xresources ~/.Xresources && \
-    rm -rf ~/.xmonad; ln -s ${DIR}/.xmonad ~/.xmonad
+    ln -s "$DIR/.Xresources" ~/.Xresources && \
+    rm -rf ~/.xmonad; ln -s "$DIR/.xmonad" ~/.xmonad
 }
 
 command -v i3 > /dev/null 2>&1 && {
-    rm -rf ~/.config/i3; ln -s ${DIR}/.config/i3 ~/.config/i3
+    rm -rf ~/.config/i3; ln -s "$DIR/.config/i3" ~/.config/i3
 }
 
 command -v chromium-browser > /dev/null 2>&1 && {
-    sudo cp chrome-extensions-policy.json /etc/chromium-browser/policies/managed/
+    sudo cp "$DIR/chrome-extensions-policy.json" /etc/chromium-browser/policies/managed/
 }
