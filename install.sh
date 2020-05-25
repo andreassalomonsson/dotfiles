90bd7cf128218edae109a8925a15ab46d34878c4
Andreas Salomonsson <andreas.k.m.salomonsson@gmail.com>
2020-05-25 20:41:08 +0200

    Adds Alacritty config

--- install.sh
+++ install.sh
@@ -2,26 +2,33 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-ln -s "$DIR/.inputrc" ~/.inputrc
-ln -s "$DIR/.vimrc" ~/.vimrc
-ln -s "$DIR/.gitconfig" ~/.gitconfig
+ln --symbolic "$DIR/.inputrc" ~/.inputrc
+ln --symbolic "$DIR/.vimrc" ~/.vimrc
+ln --symbolic "$DIR/.gitconfig" ~/.gitconfig
 
 command -v zsh > /dev/null 2>&1 && {
-    ln -s "$DIR/.zshrc" ~/.zshrc && \
-        ln -s "$DIR/.zsh" ~/.zsh
+    ln --symbolic "$DIR/.zshrc" ~/.zshrc && \
+        ln --symbolic "$DIR/.zsh" ~/.zsh
 }
 
 command -v tmux > /dev/null 2>&1 && {
-    ln -s "$DIR/.tmux.conf" ~/.tmux.conf
+    ln --symbolic "$DIR/.tmux.conf" ~/.tmux.conf
 }
 
 command -v xmonad > /dev/null 2>&1 && {
-    ln -s "$DIR/.Xresources" ~/.Xresources && \
-        rm -rf ~/.xmonad; ln -s "$DIR/.xmonad" ~/.xmonad
+    ln --symbolic "$DIR/.Xresources" ~/.Xresources && \
+        rm --recursive --force ~/.xmonad; \
+        ln --symbolic "$DIR/.xmonad" ~/.xmonad
 }
 
 command -v i3 > /dev/null 2>&1 && {
-    rm -rf ~/.config/i3; ln -s "$DIR/.config/i3" ~/.config/i3
+    rm --recursive --force ~/.config/i3; \
+        ln --symbolic "$DIR/.config/i3" ~/.config/i3
+}
+
+command -v alacritty > /dev/null 2>&1 && {
+    rm --recursive --force ~/.config/alacritty; \
+        ln --symbolic "$DIR/.config/alacritty" ~/.config/alacritty
 }
 
 (command -v chromium > /dev/null 2>&1 || command -v chromium-browser > /dev/null 2>&1) && {
